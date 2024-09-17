9189aff7d0f0d87339f866edbe5f4b6caf9f50d6
Kcokoji <kelechiokoji52@gmail.com>
2024-09-17 11:01:25 +0100

    First commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className=" flex-1 justify-center items-center">
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
